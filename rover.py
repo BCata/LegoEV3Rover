97f542d619f542821192af0bd77cf4ad0b529a65
Alex Muresan <alex.muresan07@gmail.com>
2018-10-10 17:52:30 +0200

    LED's code cleanup and upload script

--- rover.py
+++ rover.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 from ev3dev2.display import Display
 from ev3dev2._platform.ev3 import INPUT_1, INPUT_4
@@ -7,12 +8,15 @@ from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor
 from ev3dev2.motor import LargeMotor, MoveTank, OUTPUT_A, OUTPUT_D, SpeedPercent
 
 SPEED = 35
+TIME = 0.6
+
 s = Sound()
+leds = Leds()
 cs = ColorSensor()
-ts_left = TouchSensor(INPUT_1)
-ts_right = TouchSensor(INPUT_4)
 us = UltrasonicSensor()
 us.mode = 'US-DIST-CM'
+ts_left = TouchSensor(INPUT_1)
+ts_right = TouchSensor(INPUT_4)
 
 def move_both_for_seconds(percent, seconds):
     MoveTank(OUTPUT_A, OUTPUT_D).on_for_seconds(SpeedPercent(percent),
@@ -26,15 +30,14 @@ def move_both(percent):
 
 def turn_left(percent, seconds):
     LargeMotor(OUTPUT_D).on_for_seconds(SpeedPercent(percent),
-                                        seconds)
+                                        seconds, brake=False)
 
 def turn_right(percent, seconds):
     LargeMotor(OUTPUT_A).on_for_seconds(SpeedPercent(percent),
-                                        seconds)
+                                        seconds, brake=False)
 
 def detect_line():
-    color = cs.color
-    return color == 1
+    return cs.color == 1
 
 def detect_collision():
     return ts_left.is_pressed or ts_right.is_pressed
@@ -43,11 +46,15 @@ def detect_proximity():
     return us.value()/10 < 25
 
 def collision_protocol():
-    move_both_for_seconds(-SPEED, 0.6)
-    turn_left(-SPEED, 0.6)
+    leds.set_color("LEFT", "RED") 
+    leds.set_color("RIGHT", "RED")
+    move_both_for_seconds(-SPEED, TIME)
+    turn_left(-SPEED, TIME)
 
 while True:
     if detect_line() or detect_collision() or detect_proximity():
         collision_protocol()
+        leds.set_color("LEFT", "GREEN")
+        leds.set_color("RIGHT", "GREEN") 
 
     move_both(SPEED)
